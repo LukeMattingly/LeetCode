cfcbc039c9993de87b9ab53d311ed6523d1c238b
Kycool13 <mattingly.luke@yahoo.com>
2021-11-24 11:33:21 -0700

    VMWareSetPractice

---
+++ Set/VMWareSet/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VMWareSet
+{
+    public class Set
+    {
+        public int count= 0; 
+
+        public bool isEmpty()
+        {
+            return true;
+        }
+        
+        public int getSize()
+        {
+            return count;
+        }
+
+        public void Add(Object value)
+        {
+                
+        }   
+    
+    }
+}
