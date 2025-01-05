6ffa868f2d0e6fe1a98cc30392979718ee752ecb
LukeMattingly <mattingly.luke@yahoo.com>
2025-01-04 21:45:44 -0700

    adding test cases, doing it again

--- python_solutions/1.two_sum.py
+++ python_solutions/1.two_sum.py
@@ -1,10 +1,32 @@
 from typing import List
+import unittest
+from collections import defaultdict
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        lookup = {}
-        for i, n in enumerate(nums):
-            if n in lookup:
-                return [lookup[n], i]
-            lookup[target-n] = i
-        return []
+        index_map = defaultdict(int)
+        for i, num in enumerate(nums):
+            sub = target-num
+            if sub in index_map:
+                return[index_map[sub], i]
+            else:
+                index_map[num] = i   
+
+class test_twoSum(unittest.TestCase):
+    def test_twoSum_1(self):
+        solution = Solution()
+        ans = [0,1]
+        self.assertEqual(solution.twoSum([2,7,11,15], 9 ), ans)
+
+    def test_twoSum_2(self):
+        solution = Solution()
+        ans = [1,2]
+        self.assertEqual(solution.twoSum([3,2,4], 6), ans)
+
+    def test_twoSum_3(self):
+        solution = Solution()
+        ans = [0,1]
+        self.assertEqual(solution.twoSum([3,3],6), ans)
+
+if __name__ == '__main__':
+    unittest.main()
