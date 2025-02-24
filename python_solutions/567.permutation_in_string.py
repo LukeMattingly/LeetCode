71aa3681447de7f297d436a5b67ff07ec0bd8009
LukeMattingly <mattingly.luke@yahoo.com>
2025-02-23 17:56:58 -0700

    permutation in string

---
+++ python_solutions/567.permutation_in_string.py
@@ -0,0 +1,44 @@
+from typing import List
+import unittest
+from collections import defaultdict
+
+class Solution():
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq_count_s1 = defaultdict(int)
+        freq_count_window = defaultdict(int)
+
+        for s in s1:
+            freq_count_s1[s] += 1
+
+        left = 0
+        for right in range(len(s2)):
+            freq_count_window[s2[right]] += 1
+
+            #if window size exceeds s1 remove leftmovst character
+            if right - left +1 > len(s1):
+                freq_count_window[s2[left]] -=1
+                if freq_count_window[s2[left]] ==0:
+                    del freq_count_window[s2[left]]
+                left+=1
+            
+            if freq_count_window == freq_count_s1:
+                return True
+
+        return False
+
+
+class test_checkInclusion(unittest.TestCase):
+    def test_checkInclusion_1(self):
+        solution = Solution()
+        ans = True
+        self.assertEqual(solution.checkInclusion("ab", "eidbaooo"), ans)
+
+    def test_checkInclusion_2(self):
+        solution = Solution()
+        ans = False
+        self.assertEqual(solution.checkInclusion("ab", "eidboaoo"), ans)
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
