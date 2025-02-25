8d05de598b6039e2b18d725712882b023a2feded
LukeMattingly <mattingly.luke@yahoo.com>
2025-02-24 22:20:27 -0700

    generateParenthesis stack + backtrack

--- python_solutions/22.generate_parenthesis.py
+++ python_solutions/22.generate_parenthesis.py
@@ -3,7 +3,31 @@ from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        return ""
+        #only add open parenthesis if open < n
+        #only add a closing parenthesis if closed < open
+        #if and only if open == closed == n is it valid
+
+        stack = []
+        res = []
+
+        def backtrack(openCount, closedCount):
+            if openCount == closedCount == n:
+                res.append("".join(stack))
+                return
+            
+            if openCount < n:
+                stack.append("(")
+                backtrack(openCount+1, closedCount)
+                stack.pop()
+            
+            if closedCount < openCount:
+                stack.append(")")
+                backtrack(openCount, closedCount + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        
+        return res
 
 
 class test_generateParenthesis(unittest.TestCase):
