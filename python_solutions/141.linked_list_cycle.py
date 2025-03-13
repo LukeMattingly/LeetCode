ae8684bcc77a43436fdc0287761976d23969156d
LukeMattingly <mattingly.luke@yahoo.com>
2025-03-12 22:18:35 -0600

    detect cycle in linked list

---
+++ python_solutions/141.linked_list_cycle.py
@@ -0,0 +1,24 @@
+from typing import Optional
+import unittest
+from collections import defaultdict
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution():
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast, slow = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+            
+        return False 
+
+
+if __name__ == '__main__':
+    unittest.main()
