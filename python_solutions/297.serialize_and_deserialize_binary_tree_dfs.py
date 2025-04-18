2383ef9806915242686ae5d0acc050fbcbe680bf
LukeMattingly <mattingly.luke@yahoo.com>
2025-04-17 22:32:03 -0600

    use dfs and comma separate node values to encode, and decode

---
+++ python_solutions/297.serialize_and_deserialize_binary_tree_dfs.py
@@ -0,0 +1,61 @@
+from typing import List, Optional, Deque
+import unittest
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        result = []
+        def dfs(node):
+            if not node:
+                result.append("null")
+                return 
+        
+            result.append(str(node.val))
+
+            dfs(node.left)
+            dfs(node.right)
+        
+        dfs(root)
+        return ",".join(result)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        vals = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "null":
+                self.i +=1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i+=1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
+
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
+
+if __name__ == '__main__':
+    unittest.main()
