4bab45b0bbaab09c2ab53f47db316bd19869a6da
LukeMattingly <mattingly.luke@yahoo.com>
2025-05-19 23:16:52 -0600

    heapify heapq library is great

---
+++ python_solutions/703.kth_largest_element_in_a_stream.py
@@ -0,0 +1,24 @@
+from typing import List
+import unittest
+import heapq 
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = nums
+        self.k = k
+        heapq.heapify(self.heap)
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap)> self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+
+
+
+if __name__ == '__main__':
+    unittest.main()
