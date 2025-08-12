e8ffbbe65fa574c6f45e40973cd97c092f08ab4b
LukeMattingly <mattingly.luke@yahoo.com>
2025-08-11 21:20:34 -0600

    remove outdated test

--- python_solutions/206.reverse_linked_list.py
+++ python_solutions/206.reverse_linked_list.py
@@ -27,24 +27,5 @@ class Solution:
         return previous 
 
 
-        
-
-class test_twoSum(unittest.TestCase):
-    def test_twoSum_1(self):
-        solution = Solution()
-        ans = [1,2]
-        self.assertEqual(solution.twoSum([2,7,11,15], 9), ans)
-
-    def test_twoSum_2(self):
-        solution = Solution()
-        ans = [1,3]
-        self.assertEqual(solution.twoSum([2,3,4], 6), ans)
-
-    def test_twoSum_3(self):
-        solution = Solution()
-        ans = [1,2]
-        self.assertEqual(solution.twoSum([-1,0], -1), ans)
-
-
 if __name__ == '__main__':
     unittest.main()
