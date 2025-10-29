193424181ef2577bf3790d54cfcf192ddee86aad
LukeMattingly <mattingly.luke@yahoo.com>
2025-10-28 20:23:56 -0600

    jump game

---
+++ python_solutions/55.jump_game.py
@@ -0,0 +1,15 @@
+from typing import List
+import unittest
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        farthest = len(nums)-1
+        for i in range(len(nums) -1, -1, -1):
+            if i + nums[i]>= farthest:
+                farthest = i
+
+        if farthest ==0:
+            return True
+        else:
+            return False
