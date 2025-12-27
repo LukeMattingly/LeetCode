edd3c59da2ca6568396576af72197a359dd6c5e1
LukeMattingly <mattingly.luke@yahoo.com>
2025-12-26 20:29:54 -0700

    backtracking for subsetsII all possible

---
+++ python_solutions/90.subsets_II.py
@@ -0,0 +1,21 @@
+from typing import List
+import unittest
+
+class Solution():
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def backtrack(start, subset):
+            result.append(subset.copy())
+
+            for i in range(start, len(nums)):
+                if i> start and nums[i] == nums[i-1]:
+                    continue
+
+                subset.append(nums[i])
+                backtrack(i+1, subset)
+                subset.pop()
+
+        nums.sort()    
+        backtrack(0, [])
+        return result
