c2c23ba7093963f6002f2189cdd23f1a788e1c31
LukeMattingly <mattingly.luke@yahoo.com>
2026-03-06 23:56:18 -0700

    building simple search index oop

---
+++ java_solutions/SimpleSearchIndex.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SimpleSearchIndex{
+    // store docId -> document
+    private final Map<Integer, String> documents = new ConcurrentHashMap<>();
+
+    // token -> [doc1, doc2] set of docIds
+    private final Map<String, Set<Integer>> invertedIndex = new ConcurrentHashMap<>();
+
+    //Id generator thread safe
+    private final AtomicInteger docIdGenerator = new AtomicInteger();
+
+
+    // add new docId and document to documents map, and update the inverted index
+    public int addDocument(String text){
+        if(text == null || text.trim().isEmpty()) return -1;
+
+        int docId = docIdGenerator.incrementAndGet();
+
+        documents.put(docId, text);
+
+        //generate tokens
+        Set<String> tokens = tokenize(text);
+
+        for(String token: tokens){
+            //computeIfAbsent with a thread-safe Set
+            invertedIndex.computeIfAbsent(token, k-> ConcurrentHashMap.newKeySet()).add(docId);
+        }
+        return docId;
+    }
+
+    public Set<String> tokenize(String text){
+        Set<String> tokens = new HashSet<>();
+
+        String[] words = text.toLowerCase().replaceAll("[^a-zA-Z\\d\\s:]","" ).split("\\s+");
+
+        for(String word: words){
+            if(!word.isEmpty())
+                tokens.add(word);
+        }
+        return tokens;
+    }
+}
