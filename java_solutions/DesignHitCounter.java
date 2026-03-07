10c69e377d1a875c72e3ef2efeaba7c370c0dd04
LukeMattingly <mattingly.luke@yahoo.com>
2026-03-07 16:54:56 -0700

    basic hit counter

---
+++ java_solutions/DesignHitCounter.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class DesignHitCounter {
+    Deque<Integer> queue;
+    public DesignHitCounter() {
+        queue = new ArrayDeque<>();
+    }
+    
+    public void hit(int timestamp) {
+        queue.add(timestamp);
+    }
+    
+    public int getHits(int timestamp) {
+        while(!queue.isEmpty() && queue.peek()<= timestamp-300){
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
