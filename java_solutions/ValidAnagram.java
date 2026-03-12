bbac65850f56c00c557f03bb51c313e8f8955576
LukeMattingly <mattingly.luke@yahoo.com>
2026-03-11 20:52:25 -0600

    valid anagram

---
+++ java_solutions/ValidAnagram.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ValidAnagram {
+        public boolean isAnagram(String s, String t) {
+            Map<Character, Integer> sMap = new HashMap<>();
+            Map<Character, Integer> tMap = new HashMap<>();
+
+            for(int i=0; i< s.length(); i++){
+                sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0)+1);
+                tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0)+1 );
+            }
+
+            return sMap.equals(tMap);
+    }
+}
