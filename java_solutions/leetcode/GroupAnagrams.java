f2163d9f6fb400a4f7023e5f3b37676c66c9d215
LukeMattingly <mattingly.luke@yahoo.com>
2026-03-15 14:23:44 -0600

    doing grouped anagrams again

--- java_solutions/leetcode/GroupAnagrams.java
+++ java_solutions/leetcode/GroupAnagrams.java
@@ -2,16 +2,19 @@ package leetcode;
 import java.util.*;
 
 public class GroupAnagrams {
-        public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> anagramMap = new HashMap<>();
 
-        for(String word: strs){
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);
-            anagramMap.computeIfAbsent(key, k-> new ArrayList<>()).add(word);
-        }
+     public List<List<String>> groupAnagrams(String[] strs) {
         
+        Map<String, List<String>> anagramMap = new HashMap<>();
+
+        for(String str: strs){
+            char [] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+            anagramMap.computeIfAbsent(sortedStr, k->new ArrayList<String>()).add(str);
+        }
+
         return new ArrayList<>(anagramMap.values());
-    }
+
+     }
 }
