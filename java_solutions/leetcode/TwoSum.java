67667dd1f99a22373178067128846b6a8c979aaf
LukeMattingly <mattingly.luke@yahoo.com>
2026-03-15 13:24:03 -0600

    updated numsmap

--- java_solutions/leetcode/TwoSum.java
+++ java_solutions/leetcode/TwoSum.java
@@ -1,14 +1,15 @@
 package leetcode;
 import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> numsMap = new HashMap<>();
         for(int i =0; i<nums.length; i++){
-            int complement = target - nums[i];
-            if(numsMap.containsKey(complement)){
-                return new int[]{numsMap.get(complement), i};
+            if(numsMap.containsKey(nums[i])){
+                int firstIndex = numsMap.get(nums[i]);
+                return new int[]{firstIndex, i};
             }
-            numsMap.put(nums[i], i);
+            numsMap.put(target - nums[i], i);
         }
         return new int[]{};
     }
